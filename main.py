86e80d445490394134e73977b01335756f68d0df
miniusercoder <admin@miniuser.ru>
2020-05-02 22:58:14 +0300

    Little optimizations

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# coding=UTF-8
 from threading import Thread
 from gtts import gTTS
 from vk_api import VkApi
@@ -7,7 +8,7 @@ from vk_api import utils
 import os
 
 token = ""
-group = 1
+group = 0
 vk_session = VkApi(token=token)
 vk = vk_session.get_api()
 longpool = bot_longpoll.VkBotLongPoll(vk=vk_session, group_id=group)
@@ -30,10 +31,13 @@ def tts(peer_id, mid, text):
         attachment="doc" + str(owner) + "_" + str(audio_id)
     )
     os.remove(str(id) + ".mp3")
+    print("Озвучил сообщение от " + str(peer_id) + ": " + text)
 
 
 for event in longpool.listen():
     peer_id = event.message.peer_id
+    if (peer_id - 2000000000) > 0:
+        continue
     text = event.message.text
     mid = event.message.id
     vk.messages.send(
@@ -41,5 +45,5 @@ for event in longpool.listen():
         random_id=utils.get_random_id(),
         peer_id=peer_id
     )
-    thread = Thread(target=tts, args=(peer_id, mid, text))
-    thread.start()
+    Thread(target=tts, args=(peer_id, mid, text)).start()
+    print("Поставил в очередь сообщение от " + str(peer_id) + ": " + text)
